00011bbdd7859410ad9f8d1ccfbb0df37ab50a4d
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2024-03-01 15:23:14 +0100

    I added email verification

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,9 @@
 
 use App\Http\Controllers\auth\AuthController;
 use App\Http\Controllers\auth\ResetPasswordController;
+use App\Http\Controllers\auth\SendEmailVerificationEmail;
 use App\Http\Controllers\auth\SendPasswordResetLinkController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -37,8 +38,15 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/forgotpassword', SendPasswordResetLinkController::class)->name('password.email');
 Route::post('/resetpassword', ResetPasswordController::class)->name('password.reset');
 
+
+
 Route::group(['middleware' => 'api',], function ($router) {
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/me', [AuthController::class, 'me']);
+});
+
+Route::middleware('auth:api')->group(function (){
+    Route::post('/verifyemail', SendEmailVerificationEmail::class)->name('verification.email');
+    Route::get('/verification', VerifyEmailController::class)->name('verification.verify');
 });
