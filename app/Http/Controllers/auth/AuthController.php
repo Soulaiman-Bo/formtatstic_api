c37fb7fd13b9ff86088da6d5042f43296f579fcf
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2024-04-05 00:57:44 +0000

    Login And Register

--- app/Http/Controllers/auth/AuthController.php
+++ app/Http/Controllers/auth/AuthController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterUserRequest;
-use App\Repositories\AuthRepositoryInterface  ;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use App\Repositories\AuthRepositoryInterface;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
 
 class AuthController extends Controller
 {
@@ -25,5 +33,28 @@ class AuthController extends Controller
         }
 
         return response()->json(['user' => $result], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        Auth::login($user);
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json([
+            'status' => 'success',
+            'user' => new UserResource($user),
+            'access_token' => $token,
+            'token_type' => 'bearer',
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', [AuthController::class, "register"])->name("register");
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
 
 
 Route::get('/ping', function (Request  $request) {
