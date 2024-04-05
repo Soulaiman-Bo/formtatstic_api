9e9c7b229a0d333b0747d8959d1b2992731bd4e3
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2024-04-05 00:55:39 +0000

    I made Laravel Sanctum Compatible with MongoDb

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,11 @@ namespace App\Providers;
 
 use App\Repositories\AuthRepository;
 use App\Repositories\AuthRepositoryInterface;
+
 use Illuminate\Support\ServiceProvider;
+use App\Models\Sanctum\PersonalAccessToken;
+use Illuminate\Foundation\AliasLoader;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+         // Loader Alias
+    $loader = AliasLoader::getInstance();
+
+    // SANCTUM CUSTOM PERSONAL-ACCESS-TOKEN
+    $loader->alias(\Laravel\Sanctum\PersonalAccessToken::class, \App\Models\Sanctum\PersonalAccessToken::class);
     }
 }
