040e31bdb8509ec20a7b6ef4f751d49208d5e87b
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2024-04-12 17:20:42 +0000

    i create worksoace controller

---
+++ app/Models/Workspace.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Workspace extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'owner_id'
+    ];
+
+    public $timestamps = true;
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+}
